1051a0dc3f2b660344cc7351a10c733496a26a10
predrag-mitrovic-smallpdf <predrag.mitrovic@smallpdf.com>
2023-04-13 14:27:15 +0200

    Fix to enable browser sandboxes as well

--- example-signature-pad/webpack.config.js
+++ example-signature-pad/webpack.config.js
@@ -28,6 +28,9 @@ module.exports = {
     filename: '[name].js',
     path: path.resolve(__dirname, 'build')
   },
+  devServer: {
+    allowedHosts: 'all',
+  },
   plugins: [
     new HtmlWebpackPlugin({
       filename: 'index.html',
